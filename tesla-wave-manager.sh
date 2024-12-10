782c7dc5fa89d417a7ecff7afdf24588c9ba3cdd
Fred <fred@fredlouzada.com>
2024-12-09 23:26:18 -0500

    wave manager install script

--- tesla-wave-manager.sh
+++ tesla-wave-manager.sh
@@ -6,23 +6,30 @@
 # Preparing to install the Flask interface management
 echo "Installing WebApp for file management"
 
+# Home into tesla wave manager diretory
+sudo cd sudo mkdir /home/pi/tesla-wave-mgmt/
+
+# Creating web app directories
 sudo mkdir /home/pi/tesla-wave-mgmt
-cd /home/pi/tesla-wave-mgmt/
-sudo python3 -m venv .venv
-. .venv/bin/activate
-sudo pip3 install Flask
-sudo pip3 install soundfile
+sudo mkdir /home/pi/tesla-wave-mgmt/templates
 
 # Downloading files
 echo "Downloading template files and WebApp from GitHub"
+sudo wget https://raw.githubusercontent.com/iFredLouzada/USBTesla/refs/heads/main/tesla-wave-mgmt/app.py -O /home/pi/tesla-wave-mgmt/app.py
+sudo wget https://raw.githubusercontent.com/iFredLouzada/USBTesla/refs/heads/main/tesla-wave-mgmt/templates/base.html -O /home/pi/tesla-wave-mgmt/templates/base.html
+sudo wget https://raw.githubusercontent.com/iFredLouzada/USBTesla/refs/heads/main/tesla-wave-mgmt/templates/minimal.html -O /home/pi/tesla-wave-mgmt/templates/minimal.html
+sudo wget https://raw.githubusercontent.com/iFredLouzada/USBTesla/refs/heads/main/tesla-wave-mgmt/templates/modern.html -O /home/pi/tesla-wave-mgmt/templates/modern.html
 
-sudo wget https://raw.githubusercontent.com/iFredLouzada/USBTesla/refs/heads/main/tesla-wave-mgmt/app.py -O app.py
-sudo mkdir /home/pi/tesla-wave-mgmt/templates
-cd /home/pi/tesla-wave-mgmt/templates
-sudo wget https://raw.githubusercontent.com/iFredLouzada/USBTesla/refs/heads/main/tesla-wave-mgmt/templates/base.html -O base.html
-sudo wget https://raw.githubusercontent.com/iFredLouzada/USBTesla/refs/heads/main/tesla-wave-mgmt/templates/minimal.html -O minimal.html
-sudo wget https://raw.githubusercontent.com/iFredLouzada/USBTesla/refs/heads/main/tesla-wave-mgmt/templates/modern.html -O modern.html
-cd ..
+# Access directory and create .venv environment
+cd /home/pi/tesla-wave-mgmt/
+sudo python3 -m venv .venv
+
+# Activate virtual enviroment
+. .venv/bin/activate
+
+# Instal Flask and SoundFile in virtual environmnet
+sudo pip3 install flask
+sudo pip3 install soundfile
 
 # Create a service file
 echo "Creating a service file for auto start"
@@ -42,8 +49,8 @@ ExecStart=/home/pi/tesla-wave-mgmt/.venv/bin/python3 app.py
 WantedBy=multi-user.target"
 EOF'
 
-# Create a service file
-echo "Enabling web interface"
+# Enable and start the web app
 
+echo "Enabling web interface"
 sudo systemctl enable tesla-sound-manager
 sudo systemctl start tesla-sound-manager
